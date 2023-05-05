6a816a2bd8674e15006909bfde582ac77305450a
Michelle Fernandez <fernandezm525@outlook.es>
2023-05-04 20:04:19 -0500

    Prueba

---
+++ ALTA-COMPLEJIDAD-CICLOMATICA.py
@@ -0,0 +1,64 @@
+def alta_complejidad():
+    for i in range(1000):
+        for j in range(1000):
+            if i == j:
+                print("i es igual a j")
+            elif i < j:
+                print("i es menor que j")
+            else:
+                print("i es mayor que j")
+    if i == 0:
+        print("i es igual a 0")
+    elif i < 0:
+        print("i es menor que 0")
+    else:
+        print("i es mayor que 0")
+
+def alta_complejidad():
+    for i in range(1000):
+        for j in range(1000):
+            if i == j:
+                print("i es igual a j")
+            elif i < j:
+                print("i es menor que j")
+            else:
+                print("i es mayor que j")
+    if i == 0:
+        print("i es igual a 0")
+    elif i < 0:
+        print("i es menor que 0")
+    else:
+        print("i es mayor que 0")
+
+def alta_complejidad():
+    for i in range(1000):
+        for j in range(1000):
+            if i == j:
+                print("i es igual a j")
+            elif i < j:
+                print("i es menor que j")
+            else:
+                print("i es mayor que j")
+    if i == 0:
+        print("i es igual a 0")
+    elif i < 0:
+        print("i es menor que 0")
+    else:
+        print("i es mayor que 0")
+
+
+def alta_complejidad():
+    for i in range(1000):
+        for j in range(1000):
+            if i == j:
+                print("i es igual a j")
+            elif i < j:
+                print("i es menor que j")
+            else:
+                print("i es mayor que j")
+    if i == 0:
+        print("i es igual a 0")
+    elif i < 0:
+        print("i es menor que 0")
+    else:
+        print("i es mayor que 0")
